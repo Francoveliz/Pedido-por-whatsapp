98c4be9e7ab927761e86ae293b5e2bdc8bf4bda6
Francoveliz <francoleandroveliz@gmail.com>
2020-08-15 22:58:51 -0300

    enviar link a wp

--- index.js
+++ index.js
@@ -3,12 +3,3 @@ const pizza = document.getElementById("pizza");
 const empanada = document.getElementById("empanada");
 const montoTotal = document.getElementById("monto-total");
 let link = "https://wa.me/541166969916?text=";
-
-
-enviar.addEventListener("load", () => {
-
-  let pizzaPrecio = pizza.value * 50;
-  var empanadaPrecio = 5;
-  let precioTotal = pizzaPrecio + empanadaPrecio;
-  montoTotal.innerHTML = precioTotal;
-});
--- ticket.js
+++ ticket.js
@@ -1,5 +1,3 @@
-const montoTotal = document.getElementById("monto-total");
-
 function getParameterByName(name) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -7,6 +5,36 @@ function getParameterByName(name) {
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-let test = getParameterByName("empanada");
 
-montoTotal.innerHTML = test * 200;
+const montoTotal = document.getElementById("monto-total");
+
+let items = {
+  muzarellaChica: 200,
+  muzarellaGrande: 300,
+  muzarellaJamonChica: 250,
+  muzarellaJamonGrande: 350,
+  americanaChica: 375,
+  americanaGrande: 450,
+  verduraChica: 350,
+  verduraGrande: 415
+};
+
+document.getElementById("txt-paga-con").innerHTML = getParameterByName("paga-con");
+document.getElementById("txt-tu-nombre").innerHTML = getParameterByName("tu-nombre");
+document.getElementById("txt-numero-wp").innerHTML = getParameterByName("numero-wp");
+document.getElementById("txt-aclaraciones-pedido").innerHTML = getParameterByName("aclaraciones-pedido");
+document.getElementById("txt-dir-de-entrega").innerHTML = getParameterByName("dir-de-entrega");
+document.getElementById("txt-aclaraciones-entrega").innerHTML = getParameterByName("aclaraciones-entrega");
+
+
+const enviarPedido = document.getElementById("btn-enviar-pedido");
+let link = "https://wa.me/541166969916?text=";
+let test = document.getElementById("test");
+
+enviarPedido.addEventListener("click", () => {
+  let urlPedido = window.location.href;
+  urlPedido.toString();
+  let encode = encodeURIComponent(urlPedido);
+  let textoAEnviar = link + encode;
+  window.open(textoAEnviar, "Whatsapp");
+});
