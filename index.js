0d5e4ac460d85cad5d7b28e55380f4413c7c9c9d
Francoveliz <francoleandroveliz@gmail.com>
2020-08-15 21:53:08 -0300

    651

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const enviar = document.getElementById("enviar");
-const texto = document.getElementById("texto");
+const pizza = document.getElementById("pizza");
+const empanada = document.getElementById("empanada");
+const montoTotal = document.getElementById("monto-total");
 let link = "https://wa.me/541166969916?text=";
 
 
-enviar.addEventListener("submit", () => {
-  let inputValue = texto.value;
-  let encode = encodeURI(inputValue);
-  let textoAEnviar = link + encode;
-  console.log(textoAEnviar);
-  window.open(textoAEnviar, "Whatsapp");
+enviar.addEventListener("load", () => {
+
+  let pizzaPrecio = pizza.value * 50;
+  var empanadaPrecio = 5;
+  let precioTotal = pizzaPrecio + empanadaPrecio;
+  montoTotal.innerHTML = precioTotal;
 });
---
+++ ticket.js
@@ -0,0 +1,12 @@
+const montoTotal = document.getElementById("monto-total");
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+let test = getParameterByName("empanada");
+
+montoTotal.innerHTML = test * 200;
