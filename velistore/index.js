3dc8de8a0cb55a73eb9069b4e4dff5e5177361ac
Francoveliz <francoleandroveliz@gmail.com>
2020-08-27 17:31:39 -0300

    all items added

--- velistore/index.js
+++ velistore/index.js
@@ -7,10 +7,10 @@ let items = (function () {
     },
     items: [
       ["smams", [
-        ["galletita rellena chocolate", 75, ["sin tacc", " vegana"]],
+        ["galletita rellena chocolate", 75, ["sin tacc"]],
         ["galletita rellena frutilla", 75, ["sin tacc"]],
         ["galletita rellena vainilla", 75, ["sin tacc"]],
-        ["crackers sin sal clásicas", 90, ["sin tacc"]],
+        ["crackers clásicas", 90, ["sin tacc", " sin sal"]],
         ["polvoron vainilla", 82, ["sin tacc"]],
         ["polvoron coco", 82, ["sin tacc"]],
         ["polvoron limón", 82, ["sin tacc"]],
@@ -24,7 +24,7 @@ let items = (function () {
         ["premezcla universal - 1kg", 310, ["sin tacc"]]
       ]],
       ["cerro azul", [
-        ["alfajor con mousse de vainilla (vegano, sin azucar)", 78, ["sin tacc"]]
+        ["alfajor con mousse de vainilla", 78, ["sin tacc", " vegano", " sin azúcar"]]
       ]],
       ["doña pacha", [
         ["harina de arroz", 90, ["sin tacc"]],
@@ -44,9 +44,9 @@ let items = (function () {
         ["nachos - 90gr.", 75, ["sin tacc"]]
       ]],
       ["cerealsol", [
-        ["Cereal sabor miel (vegano)", 60, ["sin tacc"]],
-        ["Cereal sabor vainilla (vegano)", 60, ["sin tacc"]],
-        ["Cereal sabor chocolate (vegano)", 60, ["sin tacc"]],
+        ["Cereal sabor miel", 60, ["sin tacc", " vegano"]],
+        ["Cereal sabor vainilla", 60, ["sin tacc", " vegano"]],
+        ["Cereal sabor chocolate", 60, ["sin tacc", " vegano"]],
       ]],
       ["lennys", [
         ["budín de vainilla marmolado/chips de chocolate", 115, ["sin tacc"]],
@@ -60,7 +60,7 @@ let items = (function () {
         ["papás fritas - 70g", 55, ["sin tacc"]],
       ]],
       ["vrink", [
-        ["leche de almendras (sin azúcar) - 1L", 180, ["sin tacc"]],
+        ["leche de almendras - 1L", 180, ["sin tacc", " sin azúcar"]],
       ]],
       ["nina´s", [
         ["mantequilla de maní", 200, ["sin tacc"]],
@@ -69,14 +69,14 @@ let items = (function () {
         ["sal marina fina", 115, ["sin tacc"]],
       ]],
       ["risky dit", [
-        ["tostadas de arroz clásica, sin sal", 80, ["sin tacc"]],
+        ["tostadas de arroz clásica", 80, ["sin tacc", " sin sal"]],
       ]],
       ["arcor", [
         ["natural Break- Mix frutos secos", 75, ["sin tacc"]],
       ]],
       ["wakas", [
-        ["pastas multicereal de quinoa - 250gr.", 135, ["sin tacc"]],
-        ["pastas multicereal de chia - 250gr.", 135, ["sin tacc"]],
+        ["pastas multicereal de quinoa - 250gr.", 135, ["sin tacc", " vegano"]],
+        ["pastas multicereal de chia - 250gr.", 135, ["sin tacc", " vegano"]],
       ]],
       ["lulemu", [
         ["tostadas light", 55, ["sin tacc"]],
@@ -86,7 +86,7 @@ let items = (function () {
         ["polvo leudante - 400g", 180, ["sin tacc"]],
       ]],
       ["maní king", [
-        ["pasta de maní natural", 270, ["sin tacc"]],
+        ["pasta de maní natural", 270, ["sin tacc", " vegano"]],
       ]],
       ["franks", [
         ["pan de hamburguesa", 60, ["sin tacc"]],
@@ -95,6 +95,76 @@ let items = (function () {
         ["esencia de vainilla", 110, ["sin tacc"]],
         ["coco rallado - 50gr", 70, ["sin tacc"]],
         ["bicarbonato de sodio - 50gr", 40, ["sin tacc"]],
+      ]],
+      ["pianche", [
+        ["galletitas endulzado con Stevia sabor banana y nuez", 120, ["sin azúcar"]],
+        ["galletitas endulzado con Stevia sabor coco y avena", 120, ["sin azúcar"]],
+      ]],
+      ["meltaim", [
+        ["snack sabor : hierbas y queso", 75, ["vegano"]],
+        ["snack sabor : oliva y Orégano", 75, ["vegano"]],
+      ]],
+      ["granja del sol", [
+        ["medallón Veggies de quinoa y Espinaca", 120, ["vegano"]],
+        ["medallón Veggies de cebolla caramelizada y arroz yamani", 120, ["vegano"]],
+      ]],
+      ["lucchetti", [
+        ["milanesa de soja", 185, ["vegetariano"]],
+      ]],
+      ["granolas", [
+        ["mix clásico - avena arrollada, maní, pasas de uva, coco rallado, canela, jarabe demaíz y vainilla - 100gr.", 40, [""]],
+        ["mix energético - copos de maíz, avena, maní, semillas de girasol, lino, sesamo, pasas de uva, frutas deshidratadas, almendras, vainilla, jarabe de maíz - 100gr.", 50, [""]],
+        ["granola light - avena, miel, lino, fibra, pasas morochas, manzana deshidratada - 100gr.", 38, [""]],
+      ]],
+      ["frutos secos", [
+        ["mix Clásico - pasas negras, pasas rubias, almendras, nueces, avellanas, castañas de cajú, maní - 100gr.", 70, [""]],
+        ["nuez mariposa - 100gr.", 110, [""]],
+        ["bananas chips - 100gr.", 75, [""]],
+        ["pasas de uva - 100gr.", 28, [""]],
+        ["almendras nacionales - 100gr.", 150, [""]],
+        ["castañas de cajú - 100g", 135, [""]],
+      ]],
+      ["cereales", [
+        ["copos de maíz - 100gr.", 30, ["sin azúcar"]],
+        ["copos de maíz azucarados - 100gr.", 40, [""]],
+        ["avena Instantánea - 1Kg", 180, [""]],
+        ["avena tradicional - 1Kg", 180, [""]],
+        ["Almohaditas sabor: chocolate - 100Gr.", 50, [""]],
+        ["Almohaditas sabor: avellana - 100Gr.", 46, [""]],
+        ["Almohaditas sabor: frutilla - 100Gr.", 46, [""]],
+      ]],
+      ["harinas", [
+        ["harina integral - 1Kg.", 65, [""]],
+        ["harina de avena - 1 Kg.", 140, [""]],
+        ["fécula de mandioca - 1 Kg.", 170, [""]],
+        ["fécula de garbanzo - 1 Kg.", 110, [""]],
+      ]],
+      ["semillas", [
+        ["chía - 100Gr.", 30, [""]],
+        ["girasol - 100Gr.", 35, [""]],
+        ["lino - 100Gr.", 15, [""]],
+        ["mix - 100Gr.", 30, [""]],
+        ["sésamo integral - 100Gr.", 30, [""]],
+      ]],
+      ["legumbres", [
+        ["soja texturizada - 100Gr.", 25, [""]],
+        ["soja texturizada sustituto pollo - 250Gr.", 75, [""]],
+        ["maní salado - 100Gr.", 20, [""]],
+        ["maní - 100Gr.", 18, ["sin sal"]],
+        ["arroz yamani - 1Kg.", 170, [""]],
+      ]],
+      ["reposteria", [
+        ["cacao amargo - 100Gr.", 50, [""]],
+        ["azúcar mascabo - 500Gr.", 160, [""]],
+        ["maní salado - 100Gr.", 20, [""]],
+        ["maní - 100Gr.", 18, ["sin sal"]],
+        ["arroz yamani - 1Kg.", 170, [""]],
+      ]],
+      ["almeda", [
+        ["miel pura sólida - 480Gr.", 260, [""]],
+      ]],
+      ["san agustin", [
+        ["aceite de oliva extra virgen con aceite de girasol - 1/2L", 150, [""]],
       ]],
     ],
     setInputNames: function (array) {
