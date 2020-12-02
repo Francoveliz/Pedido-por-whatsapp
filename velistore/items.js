078828fd6ad6a42c0ade2eb31adc38b1e389297a
Francoveliz <francoleandroveliz@gmail.com>
2020-12-02 13:38:51 -0300

    Update items.js

--- velistore/items.js
+++ velistore/items.js
@@ -26,17 +26,17 @@ let items = {
   },
   {
     name: "polvoron vainilla - 130gr.",
-    price: 82,
+    price: 85,
     labels: ["sin tacc"]
   },
   {
     name: "polvoron coco - 130gr.",
-    price: 82,
+    price: 85,
     labels: ["sin tacc"]
   },
   {
     name: "polvoron limón - 130gr.",
-    price: 82,
+    price: 85,
     labels: ["sin tacc"]
   },
   {
@@ -45,20 +45,25 @@ let items = {
     labels: ["sin tacc"]
   },
   {
-    name: "pepas - 150g",
+    name: "galletitas de pepas - 150g",
     price: 65,
     labels: ["sin tacc"]
   },
+   {
+      name: "galletitas de vainilla colegial - 150g",
+      price: 65,
+      labels: ["sin tacc"]
+    },
   {
-    name: "galletitas de vainilla con chips de chocolate - 150gr.",
-    price: 135,
+    name: "galletitas de vainilla con chips de chocolate sin azúcar- 150gr.",
+    price: 130,
     labels: [""]
   },
   {
     name: "galletitas de vainilla colegial - 150gr.",
     price: 65,
     labels: ["sin tacc"]
-  },
+  }
   ],
   "santa maria": [{
     name: "premezcla universal - 1kg",
@@ -70,13 +75,13 @@ let items = {
     price: 110,
     labels: ["sin tacc"]
   },
+    {
+      name: "galletitas sabor chocolate",
+      price: 110,
+      labels: ["sin tacc"]
+    }
   ],
   "doña pacha": [{
-    name: "harina de arroz - 500gr.",
-    price: 90,
-    labels: ["sin tacc"]
-  },
-  {
     name: "premezcla de pizza - 500gr.",
     price: 145,
     labels: ["sin tacc"]
@@ -98,11 +103,6 @@ let items = {
     labels: ["sin tacc"]
   }
   ],
-  "macritas": [{
-    name: "nachos - 90gr.",
-    price: 75,
-    labels: ["sin tacc"]
-  }],
   "cerealsol": [{
     name: "Cereal sabor miel - 130gr.",
     price: 60,
@@ -120,7 +120,7 @@ let items = {
   }
   ],
   "lennys": [{
-    name: "budín de vainilla marmolado/chips de chocolate",
+    name: "budín de vainilla chips de chocolate",
     price: 115,
     labels: ["sin tacc"]
   }],
@@ -168,33 +168,28 @@ let items = {
   ],
   "nina´s": [{
     name: "mantequilla de maní",
-    price: 200,
+    price: 210,
     labels: ["sin tacc"]
   }],
   "macrozen": [{
     name: "sal marina fina - 500gr.",
-    price: 115,
+    price: 120,
     labels: ["sin tacc"]
   }],
   "risky dit": [{
-    name: "tostadas de Arroz Sin sal",
+    name: "tostadas de arroz Sin sal",
     price: 80,
     labels: ["sin tacc"]
-  },
-  {
-    name: " galletas de Arroz Sin sal",
-    price: 65,
-    labels: [""]
   }
   ],
   "wakas": [{
     name: "pastas multicereal de quinoa - 250gr.",
-    price: 135,
+    price: 145,
     labels: ["sin tacc", "vegano"]
   },
   {
     name: "pastas multicereal de chia - 250gr.",
-    price: 135,
+    price: 145,
     labels: ["sin tacc", "vegano"]
   }
   ],
@@ -225,50 +220,50 @@ let items = {
     price: 120,
     labels: ["sin azúcar"]
   },
-  {
-    name: "galletitas endulzado con Stevia sabor Limón jengibre y chía.",
-    price: 120,
-    labels: ["sin azúcar"]
-  }
   ],
   "meltaim": [{
     name: "snack sabor : hierbas y queso",
-    price: 75,
+    price: 90,
     labels: ["vegano"]
   },
   {
     name: "snack sabor : oliva y Orégano",
-    price: 75,
+    price: 90,
     labels: ["vegano"]
   }
   ],
   "granja del sol": [{
     name: "medallón Veggies de quinoa y Espinaca",
-    price: 120,
+    price: 310,
     labels: ["vegano"]
   },
   {
     name: "medallón Veggies de cebolla caramelizada y arroz yamani",
-    price: 120,
+    price: 310,
     labels: ["vegano"]
   }
   ],
 
   "granolas": [{
-    name: "mix clásico - avena arrollada, maní, pasas de uva, coco rallado, canela, jarabe demaíz y vainilla - 100gr.",
-    price: 40,
+    name: "mix clásica - avena arrollada, maní, pasas de uva, coco rallado, canela, jarabe demaíz y vainilla - 100gr.",
+    price: 45,
     labels: [""]
   },
   {
     name: "mix energético - copos de maíz, avena, maní, semillas de girasol, lino, sesamo, pasas de uva, frutas deshidratadas, almendras, vainilla, jarabe de maíz - 100gr.",
-    price: 50,
+    price: 55,
     labels: [""]
   },
   {
     name: "granola tradicional (Copos de maíz, Avena, Almendras, Maní, Manzana deshidratada y Girasol) - 100gr.",
-    price: 55,
+    price: 60,
     labels: [""]
   },
+    {
+    name: "granola especial (avena, arándanos, pasas de uva, almendras,chía, quinoa, copos de arroz, nueces, castañas de cajú, manzana deshidratada, vainilla, jarabe de maíz) - 100g.",
+    price: 70
+     labels: [""]
+    }
   ],
   "frutos secos": [{
     name: "mix Clásico - pasas negras, pasas rubias, almendras, nueces, avellanas, castañas de cajú, maní - 100gr.",
@@ -277,7 +272,7 @@ let items = {
   },
   {
     name: "nuez mariposa - 100gr.",
-    price: 110,
+    price: 130,
     labels: [""]
   },
   {
@@ -287,7 +282,7 @@ let items = {
   },
   {
     name: "pasas de uva - 100gr.",
-    price: 28,
+    price: 30,
     labels: [""]
   },
   {
@@ -296,24 +291,19 @@ let items = {
     labels: [""]
   },
   {
-    name: "castañas de cajú - 100gr.",
-    price: 135,
-    labels: [""]
-  },
-  {
-    name: "mix deportivo (Bananas chips, almendras, pasas de uva, castañas de cajú, maní tostado pelado) - 100gr.",
+    name: "mix tropical (Bananas chips, almendras, pasas de uva, castañas de cajú, maní tostado pelado, frutas abrillantadas) - 100gr.",
     price: 70,
     labels: [""]
   },
   {
-    name: "quinoa - 100gr.",
-    price: 55,
+   name: "mix sin pasas de uva (nueces, castañas de caju, mani, almendras) - 100gr.",
+     price: 100
     labels: [""]
-  },
+} 
   ],
   "cereales": [{
     name: "copos de maíz - 100gr.",
-    price: 30,
+    price: 35,
     labels: ["sin azúcar"]
   },
   {
@@ -333,22 +323,22 @@ let items = {
   },
   {
     name: "almohaditas sabor: chocolate - 100gr.",
-    price: 50,
+    price: 55,
     labels: [""]
   },
   {
     name: "almohaditas sabor: avellana - 100gr.",
-    price: 50,
+    price: 55,
     labels: [""]
   },
   {
     name: "almohaditas sabor: frutilla - 100gr.",
-    price: 50,
+    price: 55,
     labels: [""]
   },
   {
     name: "aritos frutales - 100gr.",
-    price: 40,
+    price: 45,
     labels: [""]
   },
   {
@@ -360,11 +350,11 @@ let items = {
     name: "salvado de avena - 100gr.",
     price: 17,
     labels: [""]
-  },
+  }
   ],
   "harinas": [{
     name: "harina integral - 250gr.",
-    price: 16,
+    price: 18,
     labels: [""]
   },
   {
@@ -374,7 +364,7 @@ let items = {
   },
   {
     name: "fécula de mandioca - 250gr.",
-    price: 45,
+    price: 35,
     labels: [""]
   },
   {
@@ -385,17 +375,17 @@ let items = {
   ],
   "semillas": [{
     name: "chía - 100gr.",
-    price: 30,
+    price: 35,
     labels: [""]
   },
   {
-    name: "girasol - 100Gr.",
+    name: "girasol - 100gr.",
     price: 35,
     labels: [""]
   },
   {
     name: "lino - 100gr.",
-    price: 15,
+    price: 20,
     labels: [""]
   },
   {
@@ -408,15 +398,15 @@ let items = {
     price: 30,
     labels: [""]
   }
-  ],
+ {
+   name: "quinoa - 100gr.",
+   price: 55,
+    labels: [""]
+    }],
+
   "legumbres": [{
     name: "soja texturizada - 100gr.",
     price: 25,
-    labels: [""]
-  },
-  {
-    name: "maní salado - 100gr.",
-    price: 20,
     labels: [""]
   },
   {
@@ -435,6 +425,11 @@ let items = {
     price: 55,
     labels: [""]
   },
+  {
+     name: "azúcar impalpable - 250gr.",
+      price: 40,
+      labels: [""]
+   },
   {
     name: "azúcar rubia - 250gr.",
     price: 40,
@@ -469,11 +464,7 @@ let items = {
   }
 
   ],
-  "mini arrocitas": [{
-    name: "galletas de arroz con baño de chocolate semiamargo - 53gr",
-    price: 60,
-    labels: [""]
-  }],
+
   "trevel": [{
     name: "stevia - 100ml.",
     price: 100,
@@ -506,11 +497,6 @@ let items = {
     labels: ["sin tacc"]
   },
   {
-    name: "caja de galletitas fajitas - 300gr.",
-    price: 130,
-    labels: ["sin tacc"]
-  },
-  {
     name: "Premezcla universal - 1Kg.",
     price: 200,
     labels: ["sin tacc"]
@@ -531,22 +517,6 @@ let items = {
     labels: ["sin tacc"]
   }
   ],
-  "angiola": [{
-    name: "galletitas de chocolate - 120gr.",
-    price: 125,
-    labels: ["sin tacc", "sin azúcar"]
-  },
-  {
-    name: "galletitas de Limón - 120gr.",
-    price: 105,
-    labels: ["sin tacc", "bajo en sodio"]
-  },
-  {
-    name: "galletitas de vainilla - 120gr.",
-    price: 105,
-    labels: ["sin tacc", "bajo en sodio"]
-  }
-  ],
   "celidarina": [{
     name: "harina de Maíz - 500gr.",
     price: 120,
@@ -556,7 +526,12 @@ let items = {
     name: "semillas mix rústico (Girasol- Chía- Lino- Sésamo) - 250gr.",
     price: 110,
     labels: ["sin tacc"]
-  }],
+  },
+   {
+       name: "Granola con pasas de uva - 200gr.",
+       price: 220,
+       labels: ["sin tacc"]
+    }],
   "cerro azul": [{
     name: "alfajor de cacao relleno de vainilla - 250gr.",
     price: 80,
@@ -566,11 +541,6 @@ let items = {
     name: "sal sin sodio - 70gr.",
     price: 180,
     labels: [""]
-  }],
-  "nutrizio": [{
-    name: "granolita cereales - 200gr.",
-    price: 85,
-    labels: ["sin tacc"]
   }],
   "marbe": [{
     name: "galletitas sabor Vainilla con chips de chocolate - 170gr.",
@@ -597,11 +567,7 @@ let items = {
     price: 150,
     labels: [""]
   }],
- "prueba": [{
-    name: "producto prueba",
-    price: 75,
-    labels: ["sin tacc"]
-  }],
+
   "cuarto creciente": [{
     name: "mermelada sabor Arándanos - 300gr.",
     price: 200,
@@ -612,6 +578,92 @@ let items = {
     price: 200,
     labels: [""]
   }]
-}
+ "yin yang": [{
+      name: "harina de garbanzos - 250gr.",
+      price: 75,
+      labels: ["sin tacc"]
+       },
+      {
+       name: "semillas pop (Sésamo,chía,lino,azucar organica)",
+      price: 50,
+      labels: ["sin tacc"]
+     }],
+      "Levex": [{
+      name: "Levadura instantanea - 10gr.",
+      price: 25,
+      labels: ["sin tacc"]
+       }],
+      "Néctar": [{
+      name: "miel de abejas - 340gr.",
+      price: 230,
+       labels: ["sin tacc"]
+       }],
+      "Vegetalex": [{
+       name: "medallón de verduras - 300gr.",
+       price: 190,
+       labels: [""]
+    },
+   { 
+       name: "milanesa de soja tradicional.",
+       price: 160,
+        labels: [""]
+   },
+   { 
+      name: "salchichas de soja 5u.",
+      price: 195,
+       labels: [""]
+   }],
+     "La francia": [{
+     name: "Pan rústico doble integral - 560gr.", 
+     price: 180,
+      labels: [""]
+    },
+   { 
+      name: "Pan rústico integral con calabaza - 560gr."
+      price: 180,
+       labels: [""]
+      }],
+    "Levadin": [{
+      name: "Levadura nutricional - 100gr.",
+      price: 85,
+       labels: [""]
+       }],
+    "Dos hermanos": [{ 
+      name: "Gallatas de arroz slim integral clásicas"
+      price: 75,
+       labels: ["sin tacc"]
+     }],
+   "Natural pop": [{
+     name: "Hummus instantaneo a base de harina de garbanzos- 100gr.",
+     price: 75,
+      labels: ["sin tacc, vegano"]
+      }],
+     "Kony"
+     name: "Stevia liquida - 200cc.",
+     price: 170,
+      labels: [""]
+      }],
+      "Sales": [{
+      name: "Sal de himalaya - 100gr.",
+      price: 25,
+        labels: [""]
+       }],
+     "Riera": [{
+      name: "Tostadas de arroz clásicas",
+      price: 100,
+      labels: ["sin tacc"]
+     },
+ { 
+      name: "Tostadas de arroz sin sal",
+      price: 100,
+      labels: ["sin tacc"]
+
+    }],
+    "Celienergy": [{  
+     name: "Alfajor de Nuez relleno de dulce de leche - 50gr."
+     price: 65, 
+     labels: ["sin tacc, bajo en sodio"] 
+     }],
+   }
 
 export default items;
