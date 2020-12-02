03e6ff942d3c671bcb8b403e321d7d03c9b9eb1d
Francoveliz <francoleandroveliz@gmail.com>
2020-12-02 14:51:19 -0300

    Update items.js

--- velistore/items.js
+++ velistore/items.js
@@ -685,5 +685,5 @@ let items = {
      name: "alfajor de Nuez relleno de dulce de leche - 50gr.",
      price: 65, 
      labels: ["sin tacc, bajo en sodio"] 
-     }],
+     }] 
 export default items; }
