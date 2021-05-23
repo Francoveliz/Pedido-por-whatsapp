a322eca96cba0026f6c7da4b623ccea7ed086475
Francoveliz <francoleandroveliz@gmail.com>
2021-05-22 21:23:53 -0300

    Update items.js

--- items.js
+++ items.js
@@ -8,9 +8,25 @@ let items = {
     name: "galletita rellena frutilla - 105gr.",
     price: 75,
     labels: ["sin tacc"]
-  },
+  }
 ]
 }
+"santa maria": [{
+    name: "premezcla universal - 1kg",
+    price: 310,
+    labels: ["sin tacc"]
+  },
+  {
+    name: "galletitas sabor vainilla",
+    price: 110,
+    labels: ["sin tacc"]
+  },
+    {
+      name: "galletitas sabor chocolate",
+      price: 110,
+      labels: ["sin tacc"]
+    }
+    ]
 
 
 export default items; 
