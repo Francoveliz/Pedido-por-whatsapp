2a03dc996b31ed0f8333ef13491f576cc7c2be0a
Francoveliz <francoleandroveliz@gmail.com>
2021-05-22 21:17:22 -0300

    Update items.js

--- items.js
+++ items.js
@@ -3,14 +3,14 @@ let items = {
     name: "galletita rellena chocolate - 105gr.",
     price: 75,
     labels: ["sin tacc"]
-}
-]
 },
 {
     name: "galletita rellena frutilla - 105gr.",
     price: 75,
     labels: ["sin tacc"]
   }
+]
+}
 
 
 export default items; 
